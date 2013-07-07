384329d54e9cd5a4be7d5906fc7919979348e4cd
Philip Goebel <philgoebel@gmail.com>
2013-07-07 10:46:33 +0000

    5th web.js mod

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs=require('fs');
 var buf=new Buffer (fs.readFileSync('index.html'), 'utf-8');    
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
