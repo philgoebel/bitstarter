18252e72513cc159fb8710b061583d9cba85effa
Philip Goebel <philgoebel@gmail.com>
2013-07-07 10:34:25 +0000

    modified web.js 3

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
+var fs=require('fs');
+var buf=new Buffer (size, encoding);
+var buf=new Buffer (fs.readFileSync('index.html'), 'utf-8');    
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
